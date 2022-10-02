329bb1e46e3c800c79a6aecc019460394175acab
gsofter <real.kingstar@outlook.com>
2022-10-02 10:55:46 -0500

    add theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createRenderer } from "fela";
+import { RendererProvider, ThemeProvider } from "react-fela";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import theme from "./theme";
+
+const renderer = createRenderer();
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RendererProvider renderer={renderer}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </RendererProvider>
   </React.StrictMode>
 );
 
