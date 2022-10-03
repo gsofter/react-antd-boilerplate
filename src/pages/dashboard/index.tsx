73eeee631c309f8db909014f1d97b287c678de01
gsofter <real.kingstar@outlook.com>
2022-10-02 19:24:24 -0500

    add main layout

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { MainLayout } from '../../layouts';
 
 const Dashboard: React.FC = () => {
   return (
-    <>
+    <MainLayout>
       <h1>Dashboard</h1>
-    </>
+    </MainLayout>
   );
 };
 
