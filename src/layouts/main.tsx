b217436dda00df50e8c5a9582709bbf4e268681d
gsofter <real.kingstar@outlook.com>
2022-10-02 19:03:33 -0500

    update

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Layout } from "antd";
+import React from 'react';
+import { Layout } from 'antd';
 
 const { Header, Footer, Content } = Layout;
 
@@ -9,10 +9,24 @@ interface IMainLayoutProps {
 
 export const MainLayout: React.FC<IMainLayoutProps> = ({ children }) => {
   return (
-    <Layout>
-      <Header>Header</Header>
-      <Content style={{ padding: "0 50px" }}>{children}</Content>
-      <Footer>Footer</Footer>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+            Bill is a cat.
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
     </Layout>
   );
 };
